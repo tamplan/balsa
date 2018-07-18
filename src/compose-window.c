f2aa90208b1fb9f50f3a9a13b06e255271fb416e
Peter Bloomfield <PeterBloomfield@bellsouth.net>
2018-07-18 12:51:39 -0400

    Fix margins in compose window

--- src/compose-window.c
+++ src/compose-window.c
@@ -3115,8 +3115,15 @@ create_text_area(BalsaComposeWindow *compose_window)
     compose_window->text = gtk_text_view_new();
 #endif                          /* HAVE_GTKSOURCEVIEW */
     text_view = GTK_TEXT_VIEW(compose_window->text);
+#if GTK_MR_236_IS_MERGED
     gtk_text_view_set_left_margin(text_view, 2);
     gtk_text_view_set_right_margin(text_view, 2);
+#else  /* GTK_MR_236_IS_MERGED */
+    gtk_text_view_set_left_margin(text_view, 0);
+    gtk_text_view_set_right_margin(text_view, 0);
+    gtk_widget_set_margin_start(compose_window->text, BALSA_LEFT_MARGIN);
+    gtk_widget_set_margin_end(compose_window->text, BALSA_RIGHT_MARGIN);
+#endif /* GTK_MR_236_IS_MERGED */
 
     /* set the message font */
     if (!balsa_app.use_system_fonts) {
